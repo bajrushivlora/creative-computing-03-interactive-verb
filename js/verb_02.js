7b1513cbdda7877ddba5d35d8e3eb1bf0130d22f
Vlora Bajrushi <bajrushivlora@gmail.com>
2018-10-30 08:52:00 -0400

    js

--- js/verb_02.js
+++ js/verb_02.js
@@ -1,33 +1,16 @@
-/*
-  Returns a random whole number between minimum value and maximum value
 
-  For example, calling randomNumber(0, 255);
-  will return a random whole number between 0 and 255.
+var three = document.querySelector(".three");
 
-  Source: https://stackoverflow.com/questions/1527803/generating-random-whole-numbers-in-javascript-in-a-specific-range
-*/
-function randomNumber (min, max) {
-  return Math.floor(Math.random() * (max - min + 1)) + min;
+
+function randomNumber(h1, h2, h3) {    
+  var h1 = randomNumber(0, 120);
+  var h2 = randomNumber(140, 220);
+  var h3 = randomNumber(230, 270);
+
+  var color = "hsl("h1 + h2 + h3")";
+
+     return color;
 }
 
-/*
-  Returns a random floating point number between minimum value and maximum value
-
-  For example, calling randomFloat(0, 1);
-  will return a random floating between 0.0 and 1.0.
-
-  Source: https://stackoverflow.com/questions/9724404/random-floating-point-double-in-inclusive-range
-*/
-function randomFloat (min, max) {
-  var float = Math.random();
-  var value;
-
-  if (float < 0.5) {
-    value = (1 - Math.random()) * (max-min) + min;
-  } else {
-    value = Math.random() * (max-min) + min;
-  }
-
-  return parseFloat(value.toFixed(2));
-}
+three.style.background = hslColor(h1, h2, h3);
 
