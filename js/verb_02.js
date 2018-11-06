b364ed3259dd19c70b984a75b0cc31f172a238cf
Vlora Bajrushi <bajrushivlora@gmail.com>
2018-11-05 20:22:54 -0500

    fixed

--- js/verb_02.js
+++ js/verb_02.js
@@ -1,16 +1,53 @@
 
-var three = document.querySelector(".three");
-
-
-function randomNumber(h1, h2, h3) {    
-  var h1 = randomNumber(0, 120);
-  var h2 = randomNumber(140, 220);
-  var h3 = randomNumber(230, 270);
-
-  var color = "hsl("h1 + h2 + h3")";
-
-     return color;
+/*
+  Returns a random whole number between minimum value and maximum value
+  For example, calling randomNumber(0, 255);
+  will return a random whole number between 0 and 255.
+  Source: https://stackoverflow.com/questions/1527803/generating-random-whole-numbers-in-javascript-in-a-specific-range
+*/
+function randomNumber (min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-three.style.background = hslColor(h1, h2, h3);
+/*
+  Returns a random floating point number between minimum value and maximum value
+  For example, calling randomFloat(0, 1);
+  will return a random floating between 0.0 and 1.0.
+  Source: https://stackoverflow.com/questions/9724404/random-floating-point-double-in-inclusive-range
+*/
+function randomFloat (min, max) {
+  var float = Math.random();
+  var value;
+
+  if (float < 0.5) {
+    value = (1 - Math.random()) * (max-min) + min;
+  } else {
+    value = Math.random() * (max-min) + min;
+  }
+
+  return parseFloat(value.toFixed(2));
+}
+
+/* 
+  Returns a color string in the form of "hsl(100, 50%, 50%)"
+  For example, calling color(10, 100, 40);
+  will return a string "hsl(10, 100%, 40%)"
+*/
+
+
+
+function hslColor(h, s, l) {  
+  var hue = h;
+  var saturation = s + "%";
+  var luminance = l + "%";
+
+  var color = "hsl(" + hue + "," + saturation + "," + luminance + ")";
+
+  return color;
+}
+
+var hue = randomNumber(0, 340);
+var color = hslColor(hue, 50, 80);
+var element = document.querySelector('.two');
+element.style.background = color;
 
